a1ad74ba0d78d531844f86c33dc815d401bc0154
SparkeECrash <jpark121989@gmail.com>
2020-06-12 18:00:33 -0700

    add client react app

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
 						{
 							OperatingSystem.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
 						});
+						services.AddCors(opt => 
+						{
+							opt.AddPolicy("CorsPolicy", policy => 
+							{
+								policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+							});
+						});
             services.AddControllers();
         }
 
@@ -45,6 +52,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+						app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
